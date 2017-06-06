787549de172fa8e1177e86592a192f071b57108c
liyanyan19940914 <921258351@qq.com>
2017-06-06 15:30:54 +0800

    it.skip function

--- test-demo/test/add.test.js
+++ test-demo/test/add.test.js
@@ -2,7 +2,7 @@ const add = require('../src/add.js');
 const expect = require('chai').expect;
 
 describe('should add test',function () {
-    it.only('should 1 and 1 to 2 ',function () {
+    it.skip('should 1 and 1 to 2 ',function () {
         expect(add(1,1)).to.be.equal(2);
     })
     it('should 1 and 2 to 3 ',function () {
